24d75ab0bf7ee6524402bf027c47f18588f31cdb
TitoonTheCat <titoonthecat@gmail.com>
2024-11-24 12:57:33 +0100

    l'extraterestre se déplace

--- intro.py
+++ intro.py
@@ -1,5 +1,5 @@
 alien = Actor('alien_green_walk2')
-alien.pos = 100,56
+alien.topright = 0,10
 
 WIDTH = 500
 HEIGHT = alien.height + 20
@@ -7,3 +7,8 @@ HEIGHT = alien.height + 20
 def draw():
 	screen.clear()
 	alien.draw()
+
+def update():
+	alien.left +=2
+	if alien.left > WIDTH:
+		alien.right = 0
